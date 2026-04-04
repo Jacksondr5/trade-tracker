a15a9cb5e4f2b64a5e357bf0456106b56281de7d
Jackson's Code Reviewer (bot) <code-reviewer@jackson.codes>
2026-04-03 22:37:02 -0400

    Add intentional typecheck failure for PR workflow test

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export const intentionalCompilerError: string = 123;
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
