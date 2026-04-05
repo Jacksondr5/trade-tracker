5da1264049ed17954071c951d845e8ac9f10207e
Jackson's Code Reviewer (bot) <code-reviewer@jackson.codes>
2026-04-05 12:22:24 -0400

    fix type mismatch in utils constant

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export const intentionalCompilerError: string = "123";
 export const anotherIntentionalCompilerError: boolean = false;
 export const thirdIntentionalCompilerError: Date = new Date(0);
 export const fourthIntentionalCompilerError: string[] = ["also broken"];
-export const fifthIntentionalCompilerError: number = false;
+export const fifthIntentionalCompilerError: number = 0;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
