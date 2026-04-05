753114977894ae64ca3ee286867654c56c4facb5
Jackson's Code Reviewer (bot) <code-reviewer@jackson.codes>
2026-04-05 11:56:41 -0400

    Add fourth intentional typecheck failure

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,7 @@ import { twMerge } from "tailwind-merge";
 export const intentionalCompilerError: string = "123";
 export const anotherIntentionalCompilerError: boolean = false;
 export const thirdIntentionalCompilerError: Date = new Date(0);
+export const fourthIntentionalCompilerError: string[] = "also broken";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
