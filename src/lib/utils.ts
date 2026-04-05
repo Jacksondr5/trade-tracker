599a9c9151579f468fede0b0a059b141d405a0f6
Jackson's Code Reviewer (bot) <code-reviewer@jackson.codes>
2026-04-04 21:54:25 -0400

    Fix TypeScript type mismatches in utils exports

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const intentionalCompilerError: string = 123;
-export const anotherIntentionalCompilerError: boolean = "broken";
+export const intentionalCompilerError: string = "123";
+export const anotherIntentionalCompilerError: boolean = false;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
