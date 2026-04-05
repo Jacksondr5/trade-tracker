a2f698e7cfd5a72fab2d4877ce4aefaabb3247ca
Jackson's Code Reviewer (bot) <code-reviewer@jackson.codes>
2026-04-05 11:33:25 -0400

    fix type errors in utils constants

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,9 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const intentionalCompilerError: string = 123;
+export const intentionalCompilerError: string = "123";
 export const anotherIntentionalCompilerError: boolean = false;
-export const thirdIntentionalCompilerError: Date = "still broken";
+export const thirdIntentionalCompilerError: Date = new Date(0);
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
