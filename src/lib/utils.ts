b59f38564965f9b459c9e875c8fddc7c83bc3ff3
Jackson's Code Reviewer (bot) <code-reviewer@jackson.codes>
2026-04-04 21:20:31 -0400

    fix: resolve intentional compiler mismatch in utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const intentionalCompilerError: string = 123;
+export const intentionalCompilerError: string = "123";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
