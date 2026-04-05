20440166e098c286be35e79737b04adea79aa3ef
Jackson's Code Reviewer (bot) <code-reviewer@jackson.codes>
2026-04-04 21:44:48 -0400

    Add second intentional typecheck failure

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const intentionalCompilerError: string = "123";
+export const intentionalCompilerError: string = 123;
+export const anotherIntentionalCompilerError: boolean = "broken";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
